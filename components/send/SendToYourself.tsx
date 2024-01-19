428ab4db6d3859032a226b64670b6496b582bcd6
Trymbak Mahant <trymbak0102mahant@gmail.com>
2024-01-19 16:44:58 +0300

    css improved / send transaction / swap page

--- components/send/SendToYourself.tsx
+++ components/send/SendToYourself.tsx
@@ -13,10 +13,7 @@ import TokenSelectorComponent from "../boxComps/TokenSelectorComponent";
 import { toast } from "react-toastify";
 import Image from "next/image";
 
-
-
 const SendToYourself = ({ connectedAddress, publicClient, forOthers }: any) => {
-
   const { routeVars, updateRouteVars } = useContext(RouteSelectContext);
   const {
     setBoxActionArgs,
@@ -29,7 +26,7 @@ const SendToYourself = ({ connectedAddress, publicClient, forOthers }: any) => {
   const [hash, setHash] = useState<Hex>();
 
   const [payAddress, setPayAddress] = useState<`0x${string}`>(zeroAddress);
-  const [pastedAddress, setPastedAddress] = useState<string>('');
+  const [pastedAddress, setPastedAddress] = useState<string>("");
 
   const { dstChain, dstToken } = routeVars;
   const srcToken = routeVars.srcToken;
@@ -40,17 +37,17 @@ const SendToYourself = ({ connectedAddress, publicClient, forOthers }: any) => {
       srcChain: ChainId.ETHEREUM,
       srcToken: ghoTokenMain,
     });
-  },[srcChain, srcToken, updateRouteVars]);
+  }, [srcChain, srcToken, updateRouteVars]);
 
   //use wamgi useBalance here instead
   const mainnetGHO = useBalance({
     address: connectedAddress,
     token: "0x40d16fc0246ad3160ccc09b8d0d3a2cd28ae6c2f",
     watch: true,
-    chainId: mainnet.id
+    chainId: mainnet.id,
   });
-  console.log(mainnetGHO)
-  
+  console.log(mainnetGHO);
+
   const [submitting, setSubmitting] = useState(false);
   const [submitErrorText, setSubmitErrorText] = useState("");
 
@@ -102,12 +99,12 @@ const SendToYourself = ({ connectedAddress, publicClient, forOthers }: any) => {
 
   const srcDisplay = srcCalcedVal ?? srcInputVal ?? "";
   const dstDisplay = dstCalcedVal ?? dstInputVal ?? "";
-  console.log(dstInputDebounced)
+  console.log(dstInputDebounced);
   useEffect(() => {
     const srcNum = Number(srcDisplay);
     if (srcNum > Number(mainnetGHO.data?.formatted)) {
       setSubmitErrorText(
-        "Insufficient funds. Try onramping to fill your wallet.",
+        "Insufficient funds. Try onramping to fill your wallet."
       );
     } else {
       setSubmitErrorText("");
@@ -125,48 +122,46 @@ const SendToYourself = ({ connectedAddress, publicClient, forOthers }: any) => {
     srcSpinning ||
     dstSpinning ||
     !(Number(srcInputDebounced) || Number(dstInputDebounced)) ||
-    forOthers && pastedAddress == '' ||
+    (forOthers && pastedAddress == "") ||
     submitting;
-  
+
   const confirmDisabled = !actionResponse?.tx;
 
-  useEffect(()=>{
+  useEffect(() => {
     if (forOthers) {
-      setPayAddress(`0x${pastedAddress}`!)
+      setPayAddress(`0x${pastedAddress}`!);
     } else {
-      setPayAddress(connectedAddress!)
+      setPayAddress(connectedAddress!);
     }
-  }, [pastedAddress, connectedAddress, forOthers])
+  }, [pastedAddress, connectedAddress, forOthers]);
 
   const handlePaste = async () => {
     const clipboardText = await navigator.clipboard.readText();
     if (isAddress(clipboardText)) {
       setPastedAddress(clipboardText);
     } else {
-      //toast 
-      const NotAddress = () =>
-        console.log('jhjgjhgj')
-        toast("Please Paste Valid Address...", {
-          position: 'bottom-center',
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        }
-      );
+      //toast
+      const NotAddress = () => console.log("jhjgjhgj");
+      toast("Please Paste Valid Address...", {
+        position: "bottom-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     }
   };
   const handleClearInput = () => {
-    setPastedAddress('')
+    setPastedAddress("");
   };
 
   return (
-    <main className="w-[60%] h-full items-start mt-4 flex justify-around">
+    <main className="w-[1100px] h-[600px] items-start mt-4 flex justify-around">
       <div className="bg-slate-900 w-[60%] flex h-fit flex-col pt-10  shadow-sm shadow-blue-500   items-center  rounded-2xl">
-        <div className="p-3 text-white text border-b-[1px] w-full text-2xl border-gray-300">
+        <div className="p-3 text-white  font-semibold border-b-[1px] w-full text-2xl border-gray-300">
           Convert GHO
         </div>
         <div className="p-3 m-3  w-full h-[60%] ">
@@ -181,12 +176,14 @@ const SendToYourself = ({ connectedAddress, publicClient, forOthers }: any) => {
               disabled={srcSpinning || submitting}
               placeholder="0"
             />
-            {''}<span className="text-5xl text-blue-200">{'//'}</span>{''}
+            {""}
+            <span className="text-5xl text-blue-200">{"//"}</span>
+            {""}
             <span className="flex text-white border-[1px] h-11 justify-center rounded-lg items-center w-[35%]">
-            {mainnetGHO.data?.formatted}
+              {mainnetGHO.data?.formatted}
             </span>
             <span className="flex text-white border-[1px] h-11 justify-center rounded-lg items-center w-[15%]">
-            {mainnetGHO.data?.symbol}
+              {mainnetGHO.data?.symbol}
             </span>
           </div>
 
@@ -194,7 +191,9 @@ const SendToYourself = ({ connectedAddress, publicClient, forOthers }: any) => {
           <div className="w-full h-[50%] border-b-[1px]  items-center py-8 gap-5 justify-center flex">
             {/* Respons / How Much you get */}
             <div className=" w-full   flex flex-col justify-center items-center px-3">
-              <span className="flex text-white justify-start w-full">Receving amount</span>
+              <span className="flex text-white justify-start w-full">
+                Receving amount
+              </span>
               {dstSpinning && (
                 <div className="absolute inset-0 rounded load-shine opacity-75" />
               )}
@@ -236,7 +235,9 @@ const SendToYourself = ({ connectedAddress, publicClient, forOthers }: any) => {
             Object.keys(amtInFees).map((feeName) => (
               <Fragment key={feeName}>
                 <div className="text-blue-300">{feeName}</div>
-                <div className="text-white text-right">{amtInFees[feeName]}</div>
+                <div className="text-white text-right">
+                  {amtInFees[feeName]}
+                </div>
               </Fragment>
             ))}
 
@@ -250,99 +251,115 @@ const SendToYourself = ({ connectedAddress, publicClient, forOthers }: any) => {
             ))}
         </div>
         <div className="text-red-500">{submitErrorText}</div>
-        <div className={` ${submitErrorText !='' ? 'mt-auto w-[96%] pt-5 border-b-[1px] border-white' : 'mt-auto'}`}></div>
+        <div
+          className={` ${
+            submitErrorText != ""
+              ? "mt-auto w-[96%] pt-5 border-b-[1px] border-white"
+              : "mt-auto"
+          }`}
+        ></div>
         <div className="flex w-full h-full justify-between px-3 gap-3 mt-5">
           <div className="flex w-[73%]">
-            {
-              forOthers 
-              ?(
-                <>
-                  <div className="flex flex-col gap-2 w-full">
-                    <div className="flex gap-2">
-                      <label className="text-blue-800">Paste Pay Address</label>
-                      {
-                        pastedAddress!.length >= 1 
-                        ? (
-                          <div className="cursor-pointer" onClick={handleClearInput}>
-                            <Image src='/clear.svg' alt='' width={23} height={23} />
-                          </div>
-                        )
-                        : (
-                          <div className="cursor-pointer" onClick={handlePaste}>
-                            <Image src='/copy.svg' alt='' width={23} height={23} />
-                          </div>
-                        )
-                      }
-                    </div>
-                    <input
-                      className="text-blue-300 w-full h-11 text-center"
-                      required
-                      placeholder={zeroAddress}
-                      value={pastedAddress!}
-                      onPaste={handlePaste}
-                      disabled
-                    />
-                    
+            {forOthers ? (
+              <>
+                <div className="flex flex-col gap-2 w-full">
+                  <div className="flex gap-2">
+                    <label className="text-blue-800">Paste Pay Address</label>
+                    {pastedAddress!.length >= 1 ? (
+                      <div
+                        className="cursor-pointer"
+                        onClick={handleClearInput}
+                      >
+                        <Image src="/clear.svg" alt="" width={23} height={23} />
+                      </div>
+                    ) : (
+                      <div className="cursor-pointer" onClick={handlePaste}>
+                        <Image src="/copy.svg" alt="" width={23} height={23} />
+                      </div>
+                    )}
                   </div>
-                </>
-              )
-              :(
-                <></>
-              )
-            }
+                  <input
+                    className="text-blue-300 w-full h-11 text-center"
+                    required
+                    placeholder={zeroAddress}
+                    value={pastedAddress!}
+                    onPaste={handlePaste}
+                    disabled
+                  />
+                </div>
+              </>
+            ) : (
+              <></>
+            )}
           </div>
-          <div className={`flex h-full  w-[27%] ${forOthers ? 'mt-5' : 'mt-0'}`}>
-          {showContinue ? (
-            <button
-              className={
-                `${continueDisabled ? 'bg-gray-300 text-gray-600 ' : 'bg-green-300 text-green-900 '}` +
-                "text-center font-medium" +
-                " w-full rounded-lg p-2 mt-4" +
-                " relative flex items-center justify-center"
-              }
-              onClick={() => confirmRoute({
-                chain: chain!,
-                srcChain,
-                srcToken,
-                dstToken,
-                setBoxActionArgs,
-                updateRouteVars,
-                srcInputVal: srcInputDebounced!,
-                dstInputVal: dstInputDebounced!,
-                connectedAddress,
-                continueDisabled,
-                setSubmitting,
-                setShowContinue,
-                srcDisplay,
-                recipient: payAddress //custom receiver
-              })}
-              disabled={continueDisabled}
-            >
-              Confirm Selections
-            </button>
-          ) : (
-            <button
-              className={
-                `${confirmDisabled ? 'bg-gray-300 text-gray-600 ': 'bg-purple-600 text-white'}` +
-                "text-center font-medium" +
-                " w-full rounded-lg p-2 mt-4" +
-                " relative flex items-center justify-center"
-              }
-              disabled={confirmDisabled}
-              onClick={() => executeTransaction({
-                actionResponse,
-                setSubmitting,
-                setHash,
-                setShowContinue,
-                publicClient,
-                connectedAddress, 
-                srcChain: chain?.id!,
-              })}
-            >
-              Swap
-              {submitting && <div className="absolute right-4 load-spinner"></div>}
-            </button>
-          )}
+          <div
+            className={`flex h-full  w-[27%] ${forOthers ? "mt-5" : "mt-0"}`}
+          >
+            {showContinue ? (
+              <button
+                className={
+                  `${
+                    continueDisabled
+                      ? "bg-gray-300 text-gray-600 "
+                      : "bg-green-300 text-green-900 "
+                  }` +
+                  "text-center font-medium" +
+                  " w-full rounded-lg p-2 mt-4" +
+                  " relative flex items-center justify-center"
+                }
+                onClick={() =>
+                  confirmRoute({
+                    chain: chain!,
+                    srcChain,
+                    srcToken,
+                    dstToken,
+                    setBoxActionArgs,
+                    updateRouteVars,
+                    srcInputVal: srcInputDebounced!,
+                    dstInputVal: dstInputDebounced!,
+                    connectedAddress,
+                    continueDisabled,
+                    setSubmitting,
+                    setShowContinue,
+                    srcDisplay,
+                    recipient: payAddress, //custom receiver
+                  })
+                }
+                disabled={continueDisabled}
+              >
+                Confirm Selections
+              </button>
+            ) : (
+              <button
+                className={
+                  `${
+                    confirmDisabled
+                      ? "bg-gray-300 text-gray-600 "
+                      : "bg-purple-600 text-white"
+                  }` +
+                  "text-center font-medium" +
+                  " w-full rounded-lg p-2 mt-4" +
+                  " relative flex items-center justify-center"
+                }
+                disabled={confirmDisabled}
+                onClick={() =>
+                  executeTransaction({
+                    actionResponse,
+                    setSubmitting,
+                    setHash,
+                    setShowContinue,
+                    publicClient,
+                    connectedAddress,
+                    srcChain: chain?.id!,
+                  })
+                }
+              >
+                Swap
+                {submitting && (
+                  <div className="absolute right-4 load-spinner"></div>
+                )}
+              </button>
+            )}
           </div>
         </div>
         <div className="flex items-end w-[95%] justify-center mt-10 mb-2 h-full">
@@ -362,42 +379,89 @@ const SendToYourself = ({ connectedAddress, publicClient, forOthers }: any) => {
       </div>
 
       {/* Transaction Status Card */}
-      <div className="bg-slate-900 h-[60%]  flex flex-col justify-center items-center  rounded-2xl shadow-sm shadow-blue-500  w-[35%]">
-        <div className="p-4 mt-4">
-          <h1 className="text-2xl text-center font-semibold mb-6">
+      <div className=" h-full  flex flex-col justify-start gap-7 items-center   w-[35%]">
+        <div className="p-4 rounded-2xl shadow-sm shadow-blue-500  h-[65%] min-w-[350px] bg-slate-900">
+          <h1 className="text-2xl pt-10 text-white text-center font-semibold mb-2">
             Transaction Status
           </h1>
-          <div className="container">
-            <div className="flex flex-col  grid-cols-12 text-gray-50">
+          <div className="container w-full flex justify-center">
+            <div className="flex flex-col justify-center  grid-cols-12 text-gray-50">
               <div className="flex ">
                 <div className="col-start-2 col-end-4   relative">
-                  <div className="h-full w-3 flex items-center justify-center">
-                    <div className="h-full w-[2px] bg-green-500 pointer-events-none" />
+                  <div className="h-full w-3 flex items-end justify-center">
+                    <div className="h-[50%] w-[2px] bg-green-500 pointer-events-none" />
                   </div>
                   <div className="w-3 h-3  absolute top-1/2 -mt-3 rounded-full bg-green-500 shadow text-center">
                     <i className="fas fa-check-circle text-white" />
                   </div>
                 </div>
-                <div className="bg-green-500 col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md mx-3">
-                  <h3 className="font-semibold text-m mb-1">proccessing</h3>
+                <div className="bg-green-500  w-[140px] col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md mx-3">
+                  <h3 className="font-semibold text-center text-m mb-1">
+                    GHO Boxing
+                  </h3>
                 </div>
               </div>
               <div className="flex ">
                 <div className="col-start-2 col-end-4   relative">
                   <div className="h-full w-3 flex items-center justify-center">
-                    <div className="h-full w-[2px] bg-green-500 pointer-events-none" />
+                    <div
+                      className={`h-full w-[2px] bg-green-500 pointer-events-none ${
+                        submitting ? "bg-green-500" : "bg-slate-500"
+                      }`}
+                    />
                   </div>
-                  <div className="w-3 h-3 absolute top-1/2 -mt-3 rounded-full bg-green-500 shadow text-center">
+                  <div
+                    className={`w-3 h-3  absolute top-1/2 -mt-3 rounded-full  shadow text-center  ${
+                      submitting ? "bg-green-500" : "bg-slate-500"
+                    }`}
+                  >
                     <i className="fas fa-check-circle text-white" />
                   </div>
                 </div>
-                <div className="bg-green-500 col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md mx-3">
+                <div
+                  className={` col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md mx-3 ${
+                    submitting ? "bg-green-500" : "bg-slate-500 text-black"
+                  } `}
+                >
+                  <h3 className="font-semibold text-m mb-1">Proccessing</h3>
+                </div>
+              </div>
+              <div className="flex ">
+                <div className="col-start-2 col-end-4   relative">
+                  <div className="h-full w-3 flex items-start justify-center">
+                    <div
+                      className={`h-[50%] w-[2px]  pointer-events-none ${
+                        hash ? "bg-green-500" : "bg-slate-500"
+                      }`}
+                    />
+                  </div>
+                  <div
+                    className={`w-3 h-3 absolute top-1/2 -mt-3 rounded-full shadow text-center ${
+                      hash ? "bg-green-500" : "bg-slate-500"
+                    }`}
+                  >
+                    <i className="fas fa-check-circle text-white" />
+                  </div>
+                </div>
+                <div
+                  className={` col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md mx-3 ${
+                    hash ? "bg-green-500" : "bg-slate-500 text-black"
+                  }`}
+                >
                   <h3 className="font-semibold text-m mb-1">Executed</h3>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
+        {hash && (
+          <div className=" h-[16%] rounded-2xl shadow-sm shadow-blue-500 min-h-[100px] flex justify-center items-center text-white min-w-[350px] bg-slate-900">
+            <button className="p-3 hover:bg-white hover:text-pink border-2 rounded-2xl border-white">
+              View Transaction{" "}
+            </button>
+          </div>
+        )}
       </div>
     </main>
   );
